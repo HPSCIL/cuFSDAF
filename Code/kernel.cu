0db1a90c20e515a69747aba092125d565541d312
gaohuancug <ghcug14@cug.edu.cn>
2020-10-19 20:16:59 +0800

    update

--- Code/kernel.cu
+++ Code/kernel.cu
@@ -172,7 +172,7 @@ void cuIDW(int icycle, int nThread, int scale_factor, int background_band, int n
 					for (size_t _i = index0; _i <= index1; _i++)
 					{
 						size_t iKnownPoint = _i + _j * ns_c;
-						double d = sqrtf(powf((col_c_D[iKnownPoint] - (i % ns_block)), 2) + powf((row_c_D[iKnownPoint] - (i / ns_block)), 2));
+						double d = sqrt(pow((double)(col_c_D[iKnownPoint] - (int(i) % ns_block)), 2) + pow((double)(row_c_D[iKnownPoint] - (int(i) / ns_block)), 2));
 						d = rsqrt(d);
 
 						if (abs(d) >= 1.0e-8)		//if sqrt((knownPoint[nKnownPointIndex].x - (i % ns_block))^2 + (y - yi)^2 ) <= IDWSearchRadius, ...
